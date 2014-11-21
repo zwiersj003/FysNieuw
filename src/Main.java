a8d57e3f8ffdc75550d7c0c279fa70c62c27d60a
edotjioe <edo.tjioe@gmail.com>
2014-11-21 11:13:34 +0100

    Alles

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Yassinee
+ */
+public class Main {
+    static int ICONIFIED;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
